0703b820189a6aab7bac514a7a55c82c3c8809a5
miroslaw pasko <miroslaw.pasko@gmail.com>
2025-08-30 19:08:28 +0100

    Add simple sub-page

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, mlExamplePageView
  
 urlpatterns = [
-	path("", indexPageView, name="index")
+	path("", indexPageView, name="index"),
+    path("ml-example", mlExamplePageView, name="ml-example")
 ] 
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def indexPageView(request):
     return render(request, 'index.html')
+
+def mlExamplePageView(request):
+    return render(request, 'ml-example.html')
