1eef3108497d7bd38dfe4669c7119b9984562836
miroslaw pasko <miroslaw.pasko@gmail.com>
2025-09-01 13:31:38 +0100

    Add linear regression example page

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import indexPageView, mlExamplePageView
+from .views import indexPageView, mlExamplePageView, linearRegressionView
  
 urlpatterns = [
 	path("", indexPageView, name="index"),
-    path("ml-example", mlExamplePageView, name="ml-example")
+    path("ml-example", mlExamplePageView, name="ml-example"),
+    path("ml-example/linear-regression", linearRegressionView, name="ml-linear-regression")
 ] 
--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .modules.linearRegression import LinearRegressionModel
+
+from .forms import LinearRegressionForm
 
 # Create your views here.
 def indexPageView(request):
@@ -6,3 +9,22 @@ def indexPageView(request):
 
 def mlExamplePageView(request):
     return render(request, 'ml-example.html')
+
+def linearRegressionView(request):
+    mlModel = LinearRegressionModel()
+    context = {
+        'graph' : mlModel.genGraph(),
+        'resultY' : None
+    }
+
+    if request.method == "POST":
+        form = LinearRegressionForm(request.POST)
+        if form.is_valid():
+            context['submittedX'] = form.cleaned_data["submitted_x"]
+            context['resultY'] = mlModel.genYValue(form.cleaned_data["submitted_x"])
+    else:
+        form = LinearRegressionForm()
+
+    context['form'] = form
+    
+    return render(request, 'machine-learning/linear-regression.html', context)
